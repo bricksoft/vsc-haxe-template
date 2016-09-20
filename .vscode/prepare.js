ed7443ed064c912306a8a3ee15382c51ab196141
JayJay <bricksoftapps@gmail.com>
2016-09-20 23:23:00 +0200

    fix (#14) tweaking hxmls and prepare.js

--- .vscode/prepare.js
+++ .vscode/prepare.js
@@ -2,7 +2,7 @@ var root = process.argv[2];
 var fs = require('fs');
 var content = "";
 
-root = root.substring(0,root.length-1);
+root = root.replace(/\s*$/,"");
 
 var _config = fs.readFileSync(root+'/targets.json');
 var config = JSON.parse(_config);
@@ -11,12 +11,18 @@ console.log("generating build.hxml");
 
 content += fs.readFileSync(root+'/hxml/_build.hxml').toString();
 content += "\r\n";
-config.forEach(function(element) {
-    content += fs.readFileSync(root+'/hxml/'+element+'.hxml').toString();
-    content += "\r\n";
-});
+var hasNext = false;
+for(var i = 0; i < config.length; i++){
+    var t =  fs.readFileSync(root+'/hxml/'+config[i]+'.hxml').toString();
+    if(t){
+        content += t;
+        if(config[i+1]){
+            content += "\r\n--next\r\n";
+        }
+    }
+}
 
 fs.writeFile(root+'/build.hxml', content, function(err) {
     if (err) throw err;
-    console.log("finished.");
+    console.log("build.hxml generated successfully.");
 });
